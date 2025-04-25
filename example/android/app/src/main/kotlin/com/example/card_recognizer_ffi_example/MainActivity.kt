5233870e3e4f4494925804b8194b7b142b79389a
Magomed Dadagov <grozniy.info@gmail.com>
2025-04-26 00:02:26 +0500

    init

---
+++ example/android/app/src/main/kotlin/com/example/card_recognizer_ffi_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.card_recognizer_ffi_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ ios/Classes/card_recognizer_ffi.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../card_recognizer_ffi.podspec for more information.
+#include "../../src/card_recognizer_ffi.c"
